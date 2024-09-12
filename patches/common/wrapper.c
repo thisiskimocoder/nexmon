thisiskimocoder/nexmon

commit 1f1b4e77aec339266937c2d456e7f71d88de8cbf

Added ioctl for debugging

patches/common/wrapper.c
@@ -1,4 +1,4 @@
-/***************************************************************************
+ww/***************************************************************************
  *                                                                         *
  *          ###########   ###########   ##########    ##########           *
  *         ############  ############  ############  ############          *
@@ -643,6 +643,11 @@ AT(CHIP_VER_BCM43436b0, FW_VER_9_88_0_0, 0x18DA8)
 void
 wlc_bmac_read_tsf(void *wlc_hw, unsigned int *tsf_l_ptr, unsigned int *tsf_h_ptr)
 VOID_DUMMY
+
+AT(CHIP_VER_BCM43436b0, FW_VER_9_88_0_0, 0x243C)
+void *
+hnd_debug_info_get(void)
+RETURN_DUMMY
 
 AT(CHIP_VER_BCM4339, FW_VER_ALL, 0x4DCC8)
 void *

firmwares/bcm43436b0/structs.common.h
@@ -1343,6 +1343,31 @@ struct dma32diag {  /* diag access */
     unsigned int pad;        /* reserved */
 };
 
+struct hnd_cons {
+    uint   vcons_in;
+    uint   vcons_out;
+    char   *buf;
+    uint   buf_size;
+    uint   idx;
+    uint   out_idx;
+    uint   cbuf_idx;
+    char   cbuf[];
+} __attribute__((packed));
+
+struct hnd_debug {
+    uint32  magic;
+    uint32  version;
+    uint32  fwid;
+    char    epivers[32];
+    void    *trap_ptr;
+    struct hnd_cons *console;
+    uint32  ram_base;
+    uint32  ram_size;
+    uint32  rom_base;
+    uint32  rom_size;
+    void    *event_log_top;
+} __attribute__((packed));
+
 /*
  * Host Interface Registers
  */

patches/bcm43436b0/9_88_0_0/nexmon/src/ioctl.c
@@ -55,6 +55,14 @@ wlc_ioctl_hook(struct wlc_info *wlc, int cmd, char *arg, int len, void *wlc_if)
     int ret = IOCTL_ERROR;
 
     switch (cmd) {
+        case NEX_GET_CONSOLE:
+            struct hnd_debug *hnd_debug = (struct hnd_debug *)hnd_debug_info_get();
+            if (len > 0) {
+                memcpy(arg, hnd_debug->console->buf, len);
+                ret = IOCTL_SUCCESS;
+            }
+            break;
+
         case NEX_GET_CAPABILITIES:
             if (len == 4) {
                 memcpy(arg, &capabilities, 4);

commit 94b2c932e40f69e7f5a9c5662c5bf5d72dcfb30d

fix broken wrapper.c file by removing arbitrary characters introduced with commit 1f1b4e7

patches/common/wrapper.c
@@ -1,4 +1,4 @@
-ww/***************************************************************************
+/***************************************************************************
  *                                                                         *
  *          ###########   ###########   ##########    ##########           *
  *         ############  ############  ############  ############          *