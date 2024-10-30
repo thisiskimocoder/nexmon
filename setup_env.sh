778fee7784049582f53c45ea21c6882dc4a0abc4
kimocoder <christian@aircrack-ng.org>
2024-10-30 19:37:40 +0000

    setup_env.sh: improve + fix the buildtools wrong dir

--- setup_env.sh
+++ setup_env.sh
@@ -1,5 +1,4 @@
-OLD_PWD=$(pwd)
-cd $(dirname ${BASH_SOURCE[0]})
+#!/usr/bin/env bash
 
 export ARCH=arm
 export SUBARCH=arm
@@ -11,24 +10,19 @@ export PLATFORMUNAME=$(uname -m)
 export NEXMON_ROOT=$(pwd)
 
 if [ $HOSTUNAME == "Darwin" ]; then
-    export CC=$NEXMON_ROOT/buildtools/gcc-arm-none-eabi-5_4-2016q2-osx/bin/arm-none-eabi-
-    export CCPLUGIN=$NEXMON_ROOT/buildtools/gcc-nexmon-plugin-osx/nexmon.so
-    export ZLIBFLATE="openssl zlib"
-else if [ $HOSTUNAME == "Linux" ] && [ $PLATFORMUNAME == "x86_64" ]; then
-    export CC=$NEXMON_ROOT/buildtools/gcc-arm-none-eabi-5_4-2016q2-linux-x86/bin/arm-none-eabi-
-    export CCPLUGIN=$NEXMON_ROOT/buildtools/gcc-nexmon-plugin/nexmon.so
-    export ZLIBFLATE="zlib-flate -compress"
-else if [[ $HOSTUNAME == "Linux" ]] && [[ $PLATFORMUNAME == "armv7l" || $PLATFORMUNAME == "armv6l" || $PLATFORMUNAME == "aarch64" ]]; then
-    export CC=$NEXMON_ROOT/buildtools/gcc-arm-none-eabi-5_4-2016q2-linux-armv7l/bin/arm-none-eabi-
-    export CCPLUGIN=$NEXMON_ROOT/buildtools/gcc-nexmon-plugin-arm/nexmon.so
-    export ZLIBFLATE="zlib-flate -compress"
+	export CC=$NEXMON_ROOT/buildtools/gcc-arm-none-eabi-5_4-2016q2-osx/bin/arm-none-eabi-
+	export CCPLUGIN=$NEXMON_ROOT/buildtools/gcc-nexmon-plugin-osx/nexmon.so
+elif [ $HOSTUNAME == "Linux" ] && [ $PLATFORMUNAME == "x86_64" ]; then
+	export CC=$NEXMON_ROOT/buildtools/gcc-arm-none-eabi-5_4-2016q2-linux-x86/bin/arm-none-eabi-
+	export CCPLUGIN=$NEXMON_ROOT/buildtools/gcc-nexmon-plugin/nexmon.so
+elif [[ $HOSTUNAME == "Linux" ]] && [[ $PLATFORMUNAME == "armv7l" || $PLATFORMUNAME == "armv6l" || $PLATFORMUNAME == "aarch64" ]]; then
+	export CC=$NEXMON_ROOT/buildtools/gcc-arm-none-eabi-5_4-2016q2-linux-armv7l/bin/arm-none-eabi-
+	export CCPLUGIN=$NEXMON_ROOT/buildtools/gcc-nexmon-plugin-arm/nexmon.so
 else
-    echo "Platform not supported!"
+	echo "Platform not supported!"
 fi
-fi
-fi
+
 
 export Q=@
 export NEXMON_SETUP_ENV=1
-
 cd "$OLD_PWD"
